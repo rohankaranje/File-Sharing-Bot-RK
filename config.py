543a9ed1a847b69bb339b651af660802ca99da96
rohankaranje <149205870+rohankaranje@users.noreply.github.com>
2023-10-28 03:30:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sizzuka:<VikYCHVy5ekg03UK>@cluster0.ji3ajlc.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+DB_NAME = os.environ.get("DATABASE_NAME", "sizzuka")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
@@ -38,7 +38,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI Here Are your files Dont forget to jion our channel thanks for using AnimeDekho123.blospot.com.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
